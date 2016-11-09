f809ca157d9e8f485da43ba0dbce182d603ec9a2
paulptt <paulptt@msn.com>
2016-11-08 18:02:57 -0800

    add ConsoleApplication1

---
+++ Project1/Project1/Header1.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+void RaiseBase()
+{
+	int base = 1;
+	int exp = 1;
+	int result = 1;
+	string done;
+
+	cout << "Enter base ";
+	cin >> base;
+
+	cout << "Enter exp ";
+	cin >> exp;
+
+	for (int i=0; i < exp; i++)
+	{
+		result *= base;
+		//cout << "base = " << base << ", and i =" << i << endl;
+	}
+	cout << "Result = " << result << endl;
+	cout << "Type Done to terminate ";
+	cin >> done;
+}
+
+void Query()
+{
+	char input;
+
+	cout << "Enter y, Y, n, or N" << endl;
+	cin >> input;
+
+	switch (input) {
+	case 'y':
+		cout << "You chose y" << endl;
+		break;
+	case 'Y':
+		cout << "You chose Y" << endl;
+		break;
+	case 'n':
+		cout << "You chose n" << endl;
+		break;
+	case  'N':
+		cout << "You chose N" << endl;
+		break;
+	default:
+		cout << "You didn't choose a valid option" << endl;
+}
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,53 +1,13 @@
 #include <string>
 #include <iostream>
+#include "Header1.h"
 using namespace std;
+
+void RaiseBase();
 
 int main()
 {
-	int base= 1;
-	int exp= 1;
-	int i = 0;
-	int result = 1;
-	string done;
-
-	cout << "Enter base ";
-
-	cin >> base;
-
-	cout << "Enter exp ";
-	cin >> exp;
-
-	for (; i < exp; i++)
-	{
-		result = result*base;
-		//cout << "base = " << base << ", and i =" << i << endl;
-	}
-	cout << "Result = " << result << endl;
-	cout << "Type Done to terminate ";
-	cin >> done;
-
-	// 2nd part of the code
-	char input;
-
-	cout << "Enter y, Y, n, or N" << endl;
-	cin >> input;
-
-	switch (input) {
-	case 'y':
-		cout << "You chose y" << endl;
-		break;
-	case 'Y':
-		cout << "You chose Y" << endl;
-		break;
-	case 'n':
-		cout << "You chose n" << endl;
-		break;
-	case  'N':
-		cout << "You chose N" << endl;
-		break;
-	default:
-		cout << "You didn't choose a valid option" << endl;
-	}
-
+	RaiseBase();
+	Query();
 	return 0;
 }
